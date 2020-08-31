b9e7170cb4afb1716568321758ac3cca7812d8ce
unknown <jpending@gmail.com>
2020-08-31 10:08:21 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav className="main-nav">
+        <div className="nav-item"><a href=" ">Home</a></div>
+        <div className="nav-item"><a href=" ">About</a></div>
+        <div className="nav-item"><a href=" ">How to Use</a></div>
+        <div className="nav-item"><a href=" ">Contact</a></div>
+      </nav>
+
+      <div className="main-body">
+        <h1>Welcome to JURPS</h1>
+        <form className="log-in-home">
+          <div className="input-cont">
+            <input type="text" placeholder="Username"></input>
+            <input type="text" placeholder="Password"></input>
+          </div>
+          <div className="button-cont">
+            <button type="submit">Log In</button>
+            <button type="submit">Create Account</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
