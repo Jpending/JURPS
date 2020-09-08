fe90b303d0a0865e2a55cdf13ad6769f9c029cc1
John <jpending@gmail.com>
2020-09-08 17:13:15 -0400

    configfix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://jurps-server.herokuapp.com/api',
   TOKEN_KEY: 'balthazar-lord-of-the-undead',
 }
 //https://jurps-server.herokuapp.com/
